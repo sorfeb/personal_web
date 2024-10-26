583de60ae3e7b4c10b406395e0bd161fd437dba4
sorfeb <soros21febriano@gmail.com>
2024-10-26 22:29:57 +0700

    Test .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
