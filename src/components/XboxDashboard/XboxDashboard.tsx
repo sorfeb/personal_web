92d470ad183bff90e284034647d44ba13eaf6ebc
sorfeb <soros21febriano@gmail.com>
2025-07-17 00:03:50 +0700

    hotfix: linter

--- src/components/XboxDashboard/XboxDashboard.tsx
+++ src/components/XboxDashboard/XboxDashboard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { JSX, useEffect, useState } from 'react';
 import XboxCard from '../XboxCard/card/XboxCard';
 import styles from './XboxDashboard.module.css';
 import { useAudioManager } from '../../hooks/useAudioManager';
